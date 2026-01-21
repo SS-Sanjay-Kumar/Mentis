63421275f1bfb2ffa349449a51cd7da7ea4055fa
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-01-21 11:01:08 +0530

    chore: setup scripts for deployement

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -3,4 +3,5 @@ import dotenv from 'dotenv/config.js';
 export const ENV ={
     PORT: process.env.PORT,
     MONGODB_URI: process.env.MONGODB_URI,
+    NODE_ENV: process.env.NODE_ENV,
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,25 @@
 import express from 'express';
+import path from 'path';
 
 import { ENV } from './lib/env.js';
 import { connectDB } from './lib/db.js';
 
 const app = express();
+const __dirname = path.resolve();
 
 app.get("/health", (req, res) => {
     return res.status(200).json({
         status: "ok",
     });
 })
+
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("/{*any}", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 app.listen(ENV.PORT, () => {
     console.log("Server running at port:", ENV.PORT);
