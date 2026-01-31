63556b10e5ed241ca4023df8023219ecfbb5ca9a
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-01-31 12:57:04 +0530

    feat: add ProblemPage for practicing coding problems

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ import { Toaster } from 'react-hot-toast';
 import HomePage from './pages/HomePage'
 import ProblemsPage from './pages/ProblemsPage'
 import DashboardPage from './pages/DashboardPage';
+import ProblemPage from './pages/ProblemPage';
 
 const App = () => {
   const { isSignedIn, isLoaded } = useUser();
@@ -20,9 +21,10 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={!isSignedIn ? <HomePage /> : <Navigate to="/dashboard" />} />
-        <Route path='/dashboard' element={isSignedIn ? <DashboardPage /> : <Navigate to="/" />} />
-        <Route path='/problems' element={isSignedIn ? <ProblemsPage /> : <Navigate to="/" />} />
+        <Route path='/' element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path='/dashboard' element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
+        <Route path='/problems' element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path='/problem/:id' element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
       </Routes>
       <Toaster position='top-right' />
     </>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
                     </div>
 
                     <div className="flex flex-col">
-                        <span className="font-black text-xl bg-gradient-to-r from-primary to-accent bg-clip-text text-transparent font-mono tracking-wider">
+                        <span className="font-black text-xl bg-linear-to-r from-primary to-accent bg-clip-text text-transparent font-mono tracking-wider">
                             Mentis
                         </span>
                         <span className="text-xs text-base-content/60 font-medium -mt-1">Code Together</span>
