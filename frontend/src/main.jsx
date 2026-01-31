19fc1e05ef1cadbfc97378759beb2c7be0722255
Sanjay Kumar SS <sanjay.savitha05@gmail.com>
2026-01-31 10:08:15 +0530

    chore: axios instance setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,8 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { BrowserRouter } from 'react-router'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const queryClient = new QueryClient()
+
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Clerk Publishable Key')
@@ -13,10 +19,15 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+
     <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <App />
-      </ClerkProvider>
+      <QueryClientProvider client={queryClient}>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+
+          <App />
+
+        </ClerkProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
